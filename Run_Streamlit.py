235c63ddce57559d6a167e812dd7ebea4fdfd60f
rbhopker <65057484+rbhopker@users.noreply.github.com>
2021-05-06 17:44:23 -0300

    Update Run_Streamlit.py

--- Run_Streamlit.py
+++ Run_Streamlit.py
@@ -455,9 +455,10 @@ def page3():
     pdf_file = open(pdf, 'rb')
     # print(base64.b64encode(pdf_file.read()).decode('Latin-1'))
     base64_pdf = base64.b64encode(pdf_file.read()).decode('utf-8')
-    print(base64_pdf)
+    # print(base64_pdf)
     pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="800" type="application/pdf">' 
     st.markdown(pdf_display, unsafe_allow_html=True)
+    
 
 
 main()
